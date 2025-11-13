0e10d8fbe578fdd8e21202523896e6efe2187cac
Greg Liaskas <greg.liaskas@outlook.com>
2025-11-13 07:33:22 +0200

    initial commit

---
+++ roslyn_switch_expression/SwitchExpressionAnalyzer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+
+namespace SwitchExpressionAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SwitchExpressionAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "switch_expression";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle),
+                                                                                        Resources.ResourceManager,
+                                                                                        typeof(Resources));
+
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat),
+                                                                                                Resources.ResourceManager,
+                                                                                                typeof(Resources));
+
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription),
+                                                                                              Resources.ResourceManager,
+                                                                                              typeof(Resources));
+
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId,
+                                                                                     Title,
+                                                                                     MessageFormat,
+                                                                                     Category,
+                                                                                     DiagnosticSeverity.Warning,
+                                                                                     isEnabledByDefault: true,
+                                                                                     description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+
+            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            {
+                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
