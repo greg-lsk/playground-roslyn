5b77f2cf7164293ced482f2d61061aa9c9acd8f1
Greg Liaskas <greg.liaskas@outlook.com>
2025-11-13 11:33:05 +0200

    refactor:: style

--- roslyn_switch_expression.Test/roslyn_switch_expressionUnitTests.cs
+++ roslyn_switch_expression.Test/roslyn_switch_expressionUnitTests.cs
@@ -4,41 +4,40 @@ using VerifyCS = SwitchExpressionAnalyzer.Test.CSharpCodeFixVerifier<
     SwitchExpressionAnalyzer.SwitchExpressionAnalyzer,
     SwitchExpressionAnalyzer.SwitchExpressionCodeFixProvider>;
 
+namespace SwitchExpressionAnalyzer.Test;
 
-namespace SwitchExpressionAnalyzer.Test
+
+[TestClass]
+public class RoslynSwitchExpressionUnitTest
 {
-    [TestClass]
-    public class RoslynSwitchExpressionUnitTest
+    [TestMethod]
+    public async Task ExpressionBody_NoDiagnostic()
     {
-        [TestMethod]
-        public async Task ExpressionBody_NoDiagnostic()
-        {
-            var test =
-            @"using System;
+        var test =
+        @"using System;
 
             class Program
             {
                 static void Main() => Console.WriteLine(4);
             }";
 
-            await VerifyCS.VerifyAnalyzerAsync(test);
-        }
+        await VerifyCS.VerifyAnalyzerAsync(test);
+    }
 
 
-        [TestMethod]
-        public async Task BlockBody_Diagnostic()
-        {
-            var test =
-            @"using System;
+    [TestMethod]
+    public async Task BlockBody_Diagnostic()
+    {
+        var test =
+        @"using System;
 
             class Program
             {
                 static void Main() { Console.WriteLine(4); }
             }";
 
-            var expected = VerifyCS.Diagnostic().WithLocation(5, 17).WithArguments("Main");
+        var expected = VerifyCS.Diagnostic().WithLocation(5, 17).WithArguments("Main");
 
-            await VerifyCS.VerifyAnalyzerAsync(test, expected);
-        }
+        await VerifyCS.VerifyAnalyzerAsync(test, expected);
     }
 }
